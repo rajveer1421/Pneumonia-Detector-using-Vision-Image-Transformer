1c7f18fb92f07e7bbd13deb4802e732a6c74c069
rajveer1421 <rajveergupta1455@gmail.com>
2026-02-15 11:56:01 +0530

    Configure TensorFlow settings and add CUDA environment
    Added environment variable for CUDA and configured TensorFlow threading settings.

--- app_project.py
+++ app_project.py
@@ -1,16 +1,27 @@
 from flask import Flask,render_template,request
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+import tensorflow as tf
+tf.config.threading.set_intra_op_parallelism_threads(1)
+tf.config.threading.set_inter_op_parallelism_threads(1)
+
 from tensorflow.keras.models import load_model
 from vision_transformer import VisionImageTransformer,TransformerBlock,MultiHeadSelfAttention
 from preprocessing import process_image
+
 app=Flask(__name__)
+
 model=load_model('Pneumonia_Detector_Model_2.keras', custom_objects={
     "VisionImageTransformer": VisionImageTransformer,
     "TransformerBlock": TransformerBlock,
     "MultiHeadSelfAttention": MultiHeadSelfAttention
 })
+
 @app.route('/')
 def home():
     return render_template('home1.html')
+
 @app.route('/predict',methods=['POST'])
 def predict_pneumonia():
     if 'file' not in request.files:
@@ -18,19 +29,23 @@ def predict_pneumonia():
     inp=request.files['file']
     if inp.filename==" ":
         print("No file selected")
+
     inp=process_image(inp)
-    prediction=model.predict(inp)
+
+    prediction=model(inp, training=False).numpy()
     probability=prediction[0][0]
+
     if(probability>0.5):
         label="You have Pneumonia,Get Well Soon"
     else:
         label="Congratulations you don't have pneumonia and you are reports are normal"
-    if probability<=0.35:
-        confidence="We are highly sure in this prediction"
-    if probability>=0.73:
+
+    if probability <= 0.35 or probability >= 0.73:
         confidence="We are highly sure in this prediction"
     else:
         confidence="Low confidence in this prediction, consider confirming with a medical professional."
+
     return render_template('show.html',label=label,confidence=confidence)
+
 if(__name__=='__main__'):
     app.run(debug=True)
